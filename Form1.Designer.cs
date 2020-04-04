076eb95307879ab447777fbd1b073531fca62b4b
Memelord2004 <62381451+Memelord2004@users.noreply.github.com>
2020-04-04 13:48:19 +0300

    yadebil

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,16 +1,16 @@
-﻿namespace testtst
+﻿namespace tamagoch
 {
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,27 +20,154 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Stats = new System.Windows.Forms.Button();
+            this.heal = new System.Windows.Forms.Button();
+            this.toilet = new System.Windows.Forms.Button();
+            this.eat = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(263, 146);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 146);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Stats
+            // 
+            this.Stats.Location = new System.Drawing.Point(142, 265);
+            this.Stats.Name = "Stats";
+            this.Stats.Size = new System.Drawing.Size(75, 23);
+            this.Stats.TabIndex = 1;
+            this.Stats.Text = "Статистика";
+            this.Stats.UseVisualStyleBackColor = true;
+            this.Stats.Click += new System.EventHandler(this.Stats_Click);
+            // 
+            // heal
+            // 
+            this.heal.Location = new System.Drawing.Point(401, 265);
+            this.heal.Name = "heal";
+            this.heal.Size = new System.Drawing.Size(75, 23);
+            this.heal.TabIndex = 2;
+            this.heal.Text = "Лечение";
+            this.heal.UseVisualStyleBackColor = true;
+            this.heal.Click += new System.EventHandler(this.heal_Click);
+            // 
+            // toilet
+            // 
+            this.toilet.Location = new System.Drawing.Point(401, 113);
+            this.toilet.Name = "toilet";
+            this.toilet.Size = new System.Drawing.Size(75, 23);
+            this.toilet.TabIndex = 3;
+            this.toilet.Text = "Туалет";
+            this.toilet.UseVisualStyleBackColor = true;
+            this.toilet.Click += new System.EventHandler(this.toilet_Click);
+            // 
+            // eat
+            // 
+            this.eat.Location = new System.Drawing.Point(142, 113);
+            this.eat.Name = "eat";
+            this.eat.Size = new System.Drawing.Size(75, 23);
+            this.eat.TabIndex = 4;
+            this.eat.Text = "Еда";
+            this.eat.UseVisualStyleBackColor = true;
+            this.eat.Click += new System.EventHandler(this.eat_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(571, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(571, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(571, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(571, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(628, 445);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.eat);
+            this.Controls.Add(this.toilet);
+            this.Controls.Add(this.heal);
+            this.Controls.Add(this.Stats);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Stats;
+        private System.Windows.Forms.Button heal;
+        private System.Windows.Forms.Button toilet;
+        private System.Windows.Forms.Button eat;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
+
--- Form1.cs
+++ Form1.cs
@@ -8,17 +8,162 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace testtst
+namespace tamagoch
 {
+
     public partial class Form1 : Form
     {
+        int count_sec = 0;
+        int obser = 10;
+        int satiety = 100;
+        int hp = 70;
+        int happines = 10;
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.BackColor = Color.FromArgb(0, 0, 0, 0);
+            timer1.Interval = 5000;
+            timer1.Start();
+
+
         }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void eat_Click(object sender, EventArgs e)
+        {
+            Random rnd2 = new Random();
+            int value2 = rnd2.Next(0, 3);
+            switch (value2)
+            {
+                case 0:
+                    MessageBox.Show(
+                        "Вы покормили своего питомца!",
+                        "УраКласс"
+                        );
+                    satiety = 100;
+                    label3.Text = satiety.ToString();
+                    break;
+                case 1:
+                    MessageBox.Show(
+                "Вы покормили своего питомца!",
+                            "УраКласс"
+                                    );
+                    satiety = 100;
+                    label3.Text = satiety.ToString();
+                    break;
+                case 2:
+                    MessageBox.Show(
+                        "Ваш питомец не захотел есть вашу еду",
+                                   "Вы черт"
+                                        );
+                    satiety = satiety;
+                    label3.Text = satiety.ToString();
+                    break;
+                case 3:
+                    MessageBox.Show(
+                        "Ваш питомец не захотел есть вашу еду",
+                                   "Вы черт"
+                                        );
+                    satiety = satiety;
+                    label3.Text = satiety.ToString();
+                    break;
+            }
+        }
+
+        private void Stats_Click(object sender, EventArgs e)
+        {
+
+            string text2 = "Cытость:";
+            string text3 = "Здоровье:";
+            string text4 = "Радость:";
+            MessageBox.Show(
+                text2 + satiety.ToString() + Environment.NewLine +
+                text3 + hp.ToString() + Environment.NewLine + text4 + happines.ToString(),
+                "Статистика"
+
+                 );
+        }
+
+        private void toilet_Click(object sender, EventArgs e)
+        {
+            obser = 0;
+            label2.Text = obser.ToString();
+        }
+
+        private void heal_Click(object sender, EventArgs e)
+        {
+            hp = 100;
+            label4.Text = hp.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            Random rnd = new Random();      // отнятие сытости каждые 10сек
+            int value = rnd.Next(1, 3);
+            switch (value)
+            {
+                case 1:
+                    satiety = satiety - 10;
+                    break;
+                case 2:
+                    satiety = satiety - 1;
+                    break;
+                case 3:
+                    satiety = satiety - 5;
+                    break;
+            }
+            if (obser < 100)
+            {
+                Random rnd1 = new Random();     // возможность рандомного обсёра.
+                int value1 = rnd1.Next(0, 6);
+                switch (value1)
+                {
+                    case 0:
+                        obser = obser + 10;
+                        break;
+                    case 1:
+                        obser = obser + 5;
+                        break;
+                    case 2:
+                        obser = obser + 0;
+                        break;
+                    case 3:
+                        obser = obser + 0;
+                        break;
+                    case 4:
+                        obser = obser + 10;
+                        break;
+                    case 5:
+                        obser = obser + 5;
+                        break;
+                    case 6:
+                        obser = obser + 10;
+                        break;
+                }
+            }
+            string text = "Ваш питомец обосрался" +
+              "(нажмите кнопку toilet чтобы он сходил в туалет)" ;
+            if (obser >= 100)
+            {
+                MessageBox.Show(
+                    text,
+                    "Внимание!!!",
+                    MessageBoxButtons.OK
+                    );
+                obser = 0;
+                break;
+            }
+
+
+            label3.Text = satiety.ToString();
+            label2.Text = obser.ToString();
+            label1.Text = (count_sec++).ToString();
+        }
+      
     }
-
-    
 }
-
-    
